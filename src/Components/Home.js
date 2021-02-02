f94bc0c359071c3d9773db19a298b37df6fd6341
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-02-01 20:30:48 -0800

    homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,25 @@
 import React from 'react'
-
+import './Home.css'
+import {Link} from 'react-router-dom'
 function HomePage() {
   return (
     <>
-      <h1 className="home">
-        Homep
-      </h1>
+      <div className="heading">
+        <div >
+            <p className="landingText">
+              Plan it Out!.
+            </p>
+            <Link 
+              to='/sign-up' 
+              className="getStarted" 
+              >
+              Get Started
+            </Link>
+        </div>
+      </div>
+
+
+
     </>
   )
 }
