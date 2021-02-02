16c6e377bd27a3431945769a8dd678adb3d55ca7
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-02-02 11:24:00 -0800

    home Page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,29 @@
-import React from 'react'
-import './Home.css'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import './Home.css';
+import {Link} from 'react-router-dom';
+import Typewriter from 'typewriter-effect';
+ 
 function HomePage() {
   return (
     <>
       <div className="heading">
-        <div >
+        <div>
             <p className="landingText">
-              Plan it Out!.
+              <Typewriter
+              onInit={(typewriter
+              )=>{typewriter
+              .typeString('You Can Make Your Day Count...')
+              .pauseFor(4000)
+              .deleteAll()
+              .typeString('You can be Effective and  Productive...')
+              .pauseFor(4000)
+              .deleteAll() 
+              .typeString('Just Plan it Out!...')
+              .start()}} />
             </p>
+
             <Link 
-              to='/sign-up' 
+              to='/log-in' 
               className="getStarted" 
               >
               Get Started
